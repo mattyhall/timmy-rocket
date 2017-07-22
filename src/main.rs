f6c0b136d6e38edf816adeed401ae3091d682fa0
Matthew Hall <matthew@quickbeam.me.uk>
2017-07-22 21:46:07 +0100

    Add frontend that displays the current projects

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,29 @@ extern crate rocket;
 extern crate timmy_rocket;
 #[macro_use] extern crate rocket_contrib;
 extern crate diesel;
+extern crate rocket_cors;
 
+use rocket_cors::{AllowedOrigins, AllowedHeaders};
+use rocket::http::Method;
 use diesel::prelude::*;
 use rocket_contrib::{Json, Value};
 use timmy_rocket::db_help::{init_pool, DbConn};
 use timmy_rocket::models::*;
 
 #[get("/projects")]
-fn get_projects(conn: DbConn) -> QueryResult<Json<Vec<Project>>> {
+fn get_projects(conn: DbConn) -> QueryResult<Json> {
     use timmy_rocket::schema::projects::dsl::*;
-    projects.filter(active.eq(true)).load::<Project>(&*conn).map(|proj| Json(proj))
+    projects.filter(active.eq(true)).load::<Project>(&*conn).map(|projs| Json(json!({"projects": projs})))
 }
 
 fn main() {
-    rocket::ignite().manage(init_pool()).mount("/", routes![get_projects]).launch();
+    let all_origins = AllowedOrigins::all();
+    let options = rocket_cors::Cors {
+        allowed_origins: all_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::all(),
+        allow_credentials: true,
+        ..Default::default()
+    };
+    rocket::ignite().manage(init_pool()).attach(options).mount("/", routes![get_projects]).launch();
 }
