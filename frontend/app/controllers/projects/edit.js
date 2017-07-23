d2593adf0c746dbd30caca47f317274fa35941c5
Matthew Hall <matthew@quickbeam.me.uk>
2017-07-23 22:31:26 +0100

    Add cancel button to edit page

--- frontend/app/controllers/projects/edit.js
+++ frontend/app/controllers/projects/edit.js
@@ -3,10 +3,14 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     actions: {
         save() {
-            var t = this;
             this.model.save().then(function() {
                 history.back();
             });
+        },
+
+        cancel() {
+            this.get('model').rollbackAttributes();
+            history.back();
         }
     }
 });
