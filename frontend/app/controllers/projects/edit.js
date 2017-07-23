377628f4752c621157ad3932a8417762f1af76fb
Matthew Hall <matthew@quickbeam.me.uk>
2017-07-23 23:08:25 +0100

    Show errors until model is saved

--- frontend/app/controllers/projects/edit.js
+++ frontend/app/controllers/projects/edit.js
@@ -1,15 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    errors: null,
     actions: {
         save() {
+            let t = this;
             this.model.save().then(function() {
+                t.set('errors', null);
                 history.back();
+            }, function() {
+                t.set('errors', {title: []});
+                let errors = t.get('model.errors');
+                for (var error of errors.get('title')) {
+                    t.get('errors.title').push(error.message);
+                }
             });
         },
 
         cancel() {
             this.get('model').rollbackAttributes();
+            this.set('errors', null);
             history.back();
         }
     }
