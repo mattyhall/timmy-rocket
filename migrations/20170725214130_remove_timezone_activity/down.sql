044c547ca627f295f3026846cb4efafb5a372a50
Matthew Hall <matthew@quickbeam.me.uk>
2017-07-25 22:46:25 +0100

    Remove timezone from database
    Really you only want to know what the time was where you are, so the timezone
    isn't needed.

---
+++ migrations/20170725214130_remove_timezone_activity/down.sql
@@ -0,0 +1,3 @@
+ALTER TABLE activities
+  ALTER COLUMN start_time TYPE timestamp with time zone,
+  ALTER COLUMN end_time TYPE timestamp with time zone;
---
+++ migrations/20170725214130_remove_timezone_activity/up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE activities
+      ALTER COLUMN start_time TYPE timestamp,
+      ALTER COLUMN end_time TYPE timestamp;
