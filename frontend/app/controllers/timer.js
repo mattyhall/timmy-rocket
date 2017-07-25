8d1b6ab66e3786e53de8ee42a9c23c87cc370e29
Matthew Hall <matthew@quickbeam.me.uk>
2017-07-25 16:22:41 +0100

    Add stop and cancel button to stopwatch

--- frontend/app/controllers/timer.js
+++ frontend/app/controllers/timer.js
@@ -3,35 +3,42 @@ import moment from 'moment';
 
 export default Ember.Controller.extend({
     clock: Ember.inject.service('a-clock'),
-    initial: true,
+    state: 'initial',
     start_time: null,
-    diff: Ember.computed('start_time', 'clock.time', function() {
+    end_time: null,
+    diff: Ember.computed('start_time', 'end_time', 'clock.time', function() {
         let st = this.get('start_time');
-        if (st !== null) {
-            var duration = moment.now() - st;
-            var d = moment.utc(moment.duration(duration).asMilliseconds());
-            return d;
+        let et = this.get('end_time');
+        let state = this.get('state');
+        var duration = 0;
+        if (state == 'initial') {
+            return null;
+        } else if (state == 'timing') {
+            duration = moment.now() - st;
+        } else {
+            duration = et - st;
         }
-        return null;
+        var d = moment.utc(moment.duration(duration).asMilliseconds());
+        return d;
     }),
 
     hours: Ember.computed('diff', function() {
         if (!this.get('diff')) {
-            return 0;
+            return '0';
         }
         return this.get('diff').format('H');
     }),
 
     minutes: Ember.computed('diff', function() {
         if (!this.get('diff')) {
-            return 0;
+            return '00';
         }
         return this.get('diff').format('mm');
     }),
 
     seconds: Ember.computed('diff', function() {
         if (!this.get('diff')) {
-            return 0;
+            return '00';
         }
         return this.get('diff').format('ss');
     }),
@@ -42,8 +49,19 @@ export default Ember.Controller.extend({
 
     actions: {
         start() {
-            this.set('initial', false);
+            this.set('state', 'timing');
             this.set('start_time', moment.now());
+        },
+
+        stop() {
+            this.set('state', 'stopped');
+            this.set('end_time', moment.now());
+        },
+
+        restart() {
+            this.set('state', 'initial');
+            this.set('start_time', null);
+            this.set('end_time', null);
         }
     }
 });
